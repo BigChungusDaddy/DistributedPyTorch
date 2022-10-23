74b98ed63f4ab25ae653ab1529792b783cb9c3c8
XIANGYAO TANG <u1311496@umail.utah.edu>
2022-10-23 12:43:27 -0600

    Added automation script for installation

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo su
+apt-get update
+apt-get -y install python3-pip
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
