fa384d3a95b29fa4d5f6d6688c69938fb16267dc
BigChungusDaddy <89547016+BigChungusDaddy@users.noreply.github.com>
2022-10-24 17:16:20 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ apt-get -y install python3-pip
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+mkdir -p data
+cd data
+wget -c --quiet https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar -xvzf cifar-10-python.tar.gz
+cd ..
