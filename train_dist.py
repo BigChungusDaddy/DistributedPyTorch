0cc579d350d6fdc95516af35dcbff1b6e89f569c
XIANGYAO TANG <u1311496@umail.utah.edu>
2022-10-23 20:08:58 -0600

    Changed the code

--- train_dist.py
+++ train_dist.py
@@ -1,144 +1,149 @@
-import os
 import torch
-import torch.distributed as dist
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data import DataLoader
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 
-from math import ceil
-from random import Random
-from torch.multiprocessing import Process
-from torch.autograd import Variable
-from torchvision import datasets, transforms
+import torchvision
+import torchvision.transforms as transforms
 
+import argparse
+import os
+import random
+import numpy as np
 
-class Partition(object):
-    """ Dataset-like object, but only access a subset of it. """
+def set_random_seeds(random_seed=0):
 
-    def __init__(self, data, index):
-        self.data = data
-        self.index = index
+    torch.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(random_seed)
+    random.seed(random_seed)
 
-    def __len__(self):
-        return len(self.index)
+def evaluate(model, device, test_loader):
 
-    def __getitem__(self, index):
-        data_idx = self.index[index]
-        return self.data[data_idx]
+    model.eval()
 
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-class DataPartitioner(object):
-    """ Partitions a dataset into different chuncks. """
+    accuracy = correct / total
 
-    def __init__(self, data, sizes=[0.7, 0.2, 0.1], seed=4950):
-        self.data = data
-        self.partitions = []
-        rng = Random()
-        rng.seed(seed)
-        data_len = len(data)
-        indexes = [x for x in range(0, data_len)]
-        rng.shuffle(indexes)
+    return accuracy
 
-        for frac in sizes:
-            part_len = int(frac * data_len)
-            self.partitions.append(indexes[0:part_len])
-            indexes = indexes[part_len:]
+def main():
 
-    def use(self, partition):
-        return Partition(self.data, self.partitions[partition])
-    
-def partition_dataset():
-    """ Partitioning MNIST """
-    dataset = datasets.MNIST(
-        './data',
-        train=True,
-        download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307, ), (0.3081, ))
-        ]))
-    size = dist.get_world_size()
-    # Divide the batchsize among all processes so that for each iteration the batch size is 128.
-    batchsize = int(128 / float(size))
-    # Slice the dataset into the number of processes.
-    partition_sizes = [1.0 / size for _ in range(size)]
-    partition = DataPartitioner(dataset, partition_sizes)
-    # Extract a part of the subset.
-    partition = partition.use(dist.get_rank())
-    train_set = torch.utils.data.DataLoader(
-        partition, batch_size=batchsize, shuffle=True)
-    return train_set, batchsize
+    num_epochs_default = 10
+    batch_size_default = 256 # 1024
+    learning_rate_default = 0.1
+    random_seed_default = 0
+    model_dir_default = "saved_models"
+    model_filename_default = "resnet_distributed.pth"
 
-# Create a neural network module for training
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+    # Each process runs on 1 GPU device specified by the local_rank argument.
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--local_rank", type=int, help="Local rank. Necessary for using the torch.distributed.launch utility.")
+    parser.add_argument("--num_epochs", type=int, help="Number of training epochs.", default=num_epochs_default)
+    parser.add_argument("--batch_size", type=int, help="Training batch size for one process.", default=batch_size_default)
+    parser.add_argument("--learning_rate", type=float, help="Learning rate.", default=learning_rate_default)
+    parser.add_argument("--random_seed", type=int, help="Random seed.", default=random_seed_default)
+    parser.add_argument("--model_dir", type=str, help="Directory for saving models.", default=model_dir_default)
+    parser.add_argument("--model_filename", type=str, help="Model filename.", default=model_filename_default)
+    parser.add_argument("--resume", action="store_true", help="Resume training from saved checkpoint.")
+    argv = parser.parse_args()
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
+    local_rank = argv.local_rank
+    num_epochs = argv.num_epochs
+    batch_size = argv.batch_size
+    learning_rate = argv.learning_rate
+    random_seed = argv.random_seed
+    model_dir = argv.model_dir
+    model_filename = argv.model_filename
+    resume = argv.resume
 
-def average_gradients(model):
-    size = float(dist.get_world_size())
-    for param in model.parameters():
-        dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
-        param.grad.data /= size
+    # Create directories outside the PyTorch program
+    # Do not create directory here because it is not multiprocess safe
+    '''
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    '''
 
-def run(rank, size):
-    torch.manual_seed(4950)
-    train_set, bsz = partition_dataset()
-    model = Net()
-    model = model
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+    model_filepath = os.path.join(model_dir, model_filename)
 
-    num_batches = ceil(len(train_set.dataset) / float(bsz))
-    for epoch in range(10):
-        epoch_loss = 0.0
-        for data, target in train_set:
-            data, target = Variable(data), Variable(target)
+    # We need to use seeds to make sure that the models initialized in different processes are the same
+    set_random_seeds(random_seed=random_seed)
+
+    # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+    torch.distributed.init_process_group(backend="nccl")
+    # torch.distributed.init_process_group(backend="gloo")
+
+    # Encapsulate the model on the GPU assigned to the current process
+    model = torchvision.models.resnet18(pretrained=False)
+
+    device = torch.device("cuda:{}".format(local_rank))
+    model = model.to(device)
+    ddp_model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], output_device=local_rank)
+
+    # We only save the model who uses device "cuda:0"
+    # To resume, the device for the saved model would also be "cuda:0"
+    if resume == True:
+        map_location = {"cuda:0": "cuda:{}".format(local_rank)}
+        ddp_model.load_state_dict(torch.load(model_filepath, map_location=map_location))
+
+    # Prepare dataset and dataloader
+    transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    # Data should be prefetched
+    # Download should be set to be False, because it is not multiprocess safe
+    train_set = torchvision.datasets.CIFAR10(root="data", train=True, download=False, transform=transform) 
+    test_set = torchvision.datasets.CIFAR10(root="data", train=False, download=False, transform=transform)
+
+    # Restricts data loading to a subset of the dataset exclusive to the current process
+    train_sampler = DistributedSampler(dataset=train_set)
+
+    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, sampler=train_sampler, num_workers=8)
+    # Test loader does not have to follow distributed sampling strategy
+    test_loader = DataLoader(dataset=test_set, batch_size=128, shuffle=False, num_workers=8)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-5)
+
+    # Loop over the dataset multiple times
+    for epoch in range(num_epochs):
+
+        print("Local Rank: {}, Epoch: {}, Training ...".format(local_rank, epoch))
+        
+        # Save and evaluate model routinely
+        if epoch % 10 == 0:
+            if local_rank == 0:
+                accuracy = evaluate(model=ddp_model, device=device, test_loader=test_loader)
+                torch.save(ddp_model.state_dict(), model_filepath)
+                print("-" * 75)
+                print("Epoch: {}, Accuracy: {}".format(epoch, accuracy))
+                print("-" * 75)
+
+        ddp_model.train()
+
+        for data in train_loader:
+            inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
-            output = model(data)
-            loss = F.nll_loss(output, target)
-            epoch_loss += loss.data[0]
+            outputs = ddp_model(inputs)
+            loss = criterion(outputs, labels)
             loss.backward()
-            average_gradients(model)
             optimizer.step()
-        print('Rank ',
-              dist.get_rank(), ', epoch ', epoch, ': ',
-              epoch_loss / num_batches)
-
-def init_processes(rank, size, fn, backend='gloo'):
-    """ Initialize the distributed environment. """
-    os.environ['MASTER_ADDR'] = '192.168.1.1'
-    os.environ['MASTER_PORT'] = '29500'
-    dist.init_process_group(backend, rank=rank, world_size=size)
-    fn(rank, size)
-
 
 if __name__ == "__main__":
-    size = 2
-    processes = []
-    for rank in range(size):
-        p = Process(target=init_processes, args=(rank, size, run))
-        p.start()
-        processes.append(p)
-
-    for p in processes:
-        p.join()
+    
+    main()
